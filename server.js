ee85a1f3902197e5e5fa8fa187545aece0d0d5fe
Glitch (wealthy-leo) <none>
2018-12-14 13:04:30 -0500

    first commit

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const pug = require('pug');
 
 const app = express();
+app.set('view engine', 'pug');
+
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -13,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render('../views/pug/index.pug');
   });
 
 app.listen(process.env.PORT || 3000, () => {
