98c12e401fe8d77b2f05f948d02f284037f6c14e
Glitch (wealthy-leo) <none>
2018-12-15 12:04:25 -0500

    authentication-strategies

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const mongodb     = require('mongodb').MongoClient;
-const ObjectID    = require('mongodb').ObjectID;
-const pug         = require('pug');
-const session     = require('express-session');
-const passport    = require('passport');
-const dotEnv      = require('dotenv');
+const express       = require('express');
+const bodyParser    = require('body-parser');
+const fccTesting    = require('./freeCodeCamp/fcctesting.js');
+const mongo         = require('mongodb').MongoClient;
+const ObjectID      = require('mongodb').ObjectID;
+const pug           = require('pug');
+const session       = require('express-session');
+const passport      = require('passport');
+const LocalStrategy = require('passport-local');
+const dotEnv        = require('dotenv');
 dotEnv.config();
-
-
 const app = express();
+
+
+
+
+
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
+
 app.use(session(
   {
     secret: process.env.SESSION_SECRET,
@@ -22,14 +33,7 @@ app.use(session(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.set('view engine');
-
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-mongodb.connect(process.env.DATABASE, (err, db) =>
+mongo.connect(process.env.DATABASE, (err, db) =>
 {
   if(err)
   {
@@ -37,19 +41,12 @@ mongodb.connect(process.env.DATABASE, (err, db) =>
   }
   else
   {
-
-app.route('/')
-  .get((req, res) => {
-    res.render(process.cwd() + '/views/pug/index.pug', {title: 'Hello', message: 'Please Login'});
-  });
-
-
-
     console.log('Successful database connection')
     passport.serializeUser((user, done) =>
     {
       done(null, user._id);
     });
+
     passport.deserializeUser((id, done) =>
     {
       db.collection('users').findOne(
@@ -60,6 +57,30 @@ app.route('/')
         }
       );
     });
+
+    passport.use(new LocalStrategy(
+      (username, password, done) =>
+      {
+        db.collection('users').findOne({username: username}, 
+        (err, user) =>
+        {
+          console.log('User ' + username + ' attempted to log in.');
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          if (password !== user.password) { return done(null, false); }
+          return done(null, user);
+        });
+      }
+    ));
+
+    app.route('/')
+      .get((req, res) => 
+      {
+        res.render(process.cwd() + '/views/pug/index.pug', {title: 'Hello', message: 'Please Login'});
+      });
+    
+      console.log(db);
+
     app.listen(process.env.PORT || 3000, () => {
       console.log("Listening on port " + (process.env.PORT || 3000));
     });
