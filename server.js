b9335a8cab1121ed979ddc301e21bfec1e4beb2e
Glitch (wealthy-leo) <none>
2018-12-15 18:11:25 -0500

    logging-a-user-out

--- server.js
+++ server.js
@@ -104,13 +104,27 @@ mongo.connect(process.env.DATABASE, (err, db) =>
       );
 
     app.route('/profile')
-        .get(
-          ensureAuthenticated,
-          (req, res) =>
-          {
-            res.render(process.cwd() + '/views/pug/profile.pug', {username: req.user.username});
-          }
-        );
+      .get(
+        ensureAuthenticated,
+        (req, res) =>
+        {
+          res.render(process.cwd() + '/views/pug/profile.pug', {username: req.user.username});
+        }
+      );
+    app.route('/logout')
+      .get((req, res) =>
+      {
+        req.logout();
+        res.redirect('/');
+      }
+    );
+
+    app.use((req, res, next) =>
+    {
+      res.status(404)
+        .type('text')
+        .send('Not Found');
+    });
     app.listen(process.env.PORT || 3000, () => {
       console.log("Listening on port " + (process.env.PORT || 3000));
     });
